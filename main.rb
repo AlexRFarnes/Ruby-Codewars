def9c3561198b1d9d0ac87ebe9fabe4deb87ec31
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-06-06 02:03:59 +0000

    Complete codewars challenge 'Bouncing Balls'

--- main.rb
+++ main.rb
@@ -953,3 +953,51 @@ p comp(
 p comp(
   [121, 121, 144, 19, 161, 19, 144, 19, 11], 
   [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]) # => false
+
+
+# Bouncing Balls
+
+# A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+
+# He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+# His mother looks out of a window 1.5 meters from the ground.
+
+# How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+# Three conditions must be met for a valid experiment:
+
+#     Float parameter "h" in meters must be greater than 0
+#     Float parameter "bounce" must be greater than 0 and less than 1
+#     Float parameter "window" must be less than h.
+
+# If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+# Note:
+
+# The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+# Examples:
+
+# - h = 3, bounce = 0.66, window = 1.5, result is 3
+
+# - h = 3, bounce = 1, window = 1.5, result is -1 
+
+# (Condition 2) not fulfilled).
+
+def bouncingBall(h, bounce, window)
+  return -1 unless h > 0 && bounce > 0 && bounce < 1 && window < h
+  views = 0
+  # if height is greater than window then she sees the ball falling down
+  while h > window
+    views += 1
+    h *= bounce
+    # after the bounce if the height is greater than the window she sees the ball bouncing up
+    views += 1 if h > window
+  end
+  views
+end
+
+p bouncingBall(3, 0.66, 1.5) # => 3
+p bouncingBall(30, 0.66, 1.5) # => 15
+p bouncingBall(30, 0.75, 1.5) # => 21
+p bouncingBall(30, 0.4, 10) # => 3
+p bouncingBall(40, 1, 10) # => -1
+p bouncingBall(-5, 0.66, 1.5) # => -1
