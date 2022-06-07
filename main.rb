2b9263fb276e5ee3984862a1472fbc88c5b1b2b5
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-06-07 02:56:08 +0000

    Complete Codewars challenge 'Does my number look big in this?'

--- main.rb
+++ main.rb
@@ -1001,3 +1001,45 @@ p bouncingBall(30, 0.75, 1.5) # => 21
 p bouncingBall(30, 0.4, 10) # => 3
 p bouncingBall(40, 1, 10) # => -1
 p bouncingBall(-5, 0.66, 1.5) # => -1
+
+
+# Does my number look big in this?
+
+# A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+# For example, take 153 (3 digits), which is narcisstic:
+
+#     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+# and 1652 (4 digits), which isn't:
+
+#     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+
+# The Challenge:
+
+# Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10. This may be True and False in your language, e.g. PHP.
+
+# Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+
+def narcissistic?(value)
+  exp = value.to_s.length
+  value == value
+              .to_s
+              .chars
+              .map(&:to_i)
+              .reduce(0) { |sum, num| sum += num**exp } 
+end
+
+# def narcissistic?(value)
+#   value == value
+#               .to_s
+#               .chars
+#               .map { |num| num.to_i ** value.to_s.length }
+#               .reduce()
+# end
+
+p narcissistic?(5) # => true 
+  
+p narcissistic?(153) # => true
+
+p narcissistic?(1633) # => false
