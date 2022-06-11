e7f4ff5eb1fa8385da65ec890eedff58222f8301
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-06-11 08:17:22 +0000

    Complete codewars challenge 'Is a number prime?'

--- main.rb
+++ main.rb
@@ -1168,3 +1168,64 @@ p triangular(2) # => 3
 p triangular(4) # => 10 
 p triangular(-5) # => 0
 
+
+# Is a number prime?
+
+# Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+
+# Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+# Requirements
+
+#     You can assume you will be given an integer input.
+#     You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
+#     NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
+
+# Example
+
+# is_prime(1)  /* false */
+# is_prime(2)  /* true  */
+# is_prime(-1) /* false */
+
+
+def isPrime(num)
+  return false if num < 0
+  return true if num == 2
+  m = (num ** 0.5).ceil.to_f
+  while(m > 1)
+    if (num / m) % 1 == 0 then return false end
+    m -= 1.0
+  end
+  true
+end
+
+
+require 'prime'
+
+def isPrimeBuiltIn(num)
+  num.prime?
+end
+
+p isPrime(4) # => false
+p isPrime(100) # => false
+p isPrime(999) # => false
+p isPrime(958297) # => false
+p isPrime(-7) # => false
+p isPrime(7) # => true
+p isPrime(61) # => true
+p isPrime(89) # => true
+p isPrime(17) # => true
+p isPrime(2) # => true
+p isPrime(3) # => true
+
+
+p isPrimeBuiltIn(4) # => false
+p isPrimeBuiltIn(100) # => false
+p isPrimeBuiltIn(999) # => false
+p isPrimeBuiltIn(958297) # => false
+p isPrimeBuiltIn(-7) # => false
+p isPrimeBuiltIn(7) # => true
+p isPrimeBuiltIn(61) # => true
+p isPrimeBuiltIn(89) # => true
+p isPrimeBuiltIn(17) # => true
+p isPrimeBuiltIn(2) # => true
+p isPrimeBuiltIn(3) # => true
