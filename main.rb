5c9708c473e1f37991aa4c8b4443af68e7e56d18
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-06-15 03:29:58 +0000

    Complete codewars challenge 'Take a Ten Minutes Walk'

--- main.rb
+++ main.rb
@@ -1347,8 +1347,40 @@ p max_sequence([-2, 1, -7, 4, -10, 2, 1, 5, 4]) # => 12
 #   .strip
 # end
 
-def decodeMorse(morseCode)
-  morseCode.split("   ").map { |word| word.split(" ").map { |char| MORSE_CODE[char] }.join("") }.join(" ").strip
+# def decodeMorse(morseCode)
+#   morseCode.split("   ").map { |word| word.split(" ").map { |char| MORSE_CODE[char] }.join("") }.join(" ").strip
+# end
+
+# p decodeMorse('.... . -.--   .--- ..- -.. .') #=> 'HEY JUDE'
+
+
+
+# Take a Ten Minutes Walk
+
+# You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+    # Note: you will always receive a valid array (string in COBOL) containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+def is_valid_walk(walk)
+  return false if walk.size != 10
+  walk_coordinates = {"x"=> 0, "y"=> 0}
+  walk.each do |dir|
+    case dir
+      when "n"
+      walk_coordinates["y"] += 1
+      when "s"
+      walk_coordinates["y"] -= 1
+      when "e"
+      walk_coordinates["x"] += 1
+      when "w"
+      walk_coordinates["x"] -= 1
+    end
+  end
+  walk_coordinates["x"] == 0 && walk_coordinates["y"] == 0
 end
 
-p decodeMorse('.... . -.--   .--- ..- -.. .') #=> 'HEY JUDE'
+
+p is_valid_walk(['n','s','n','s','n','s','n','s','n','s']) # => 'true'
+p is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e']) # => 'false'
+p is_valid_walk(['w']) # => 'false'
+p is_valid_walk(['n','n','n','s','n','s','n','s','n','s']) # => 'false'
