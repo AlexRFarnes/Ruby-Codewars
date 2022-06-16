0fdaafb36a866ab0ce037c159ade75e1c7b738d7
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-06-16 02:49:10 +0000

    Complete codewars challenge 'SantaClausable Interface'

--- main.rb
+++ main.rb
@@ -1384,3 +1384,68 @@ p is_valid_walk(['n','s','n','s','n','s','n','s','n','s']) # => 'true'
 p is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e']) # => 'false'
 p is_valid_walk(['w']) # => 'false'
 p is_valid_walk(['n','n','n','s','n','s','n','s','n','s']) # => 'false'
+
+
+# SantaClausable Interface
+
+# You probably know, that in Javascript (and also Ruby) there is no concept of interfaces. There is only a concept of inheritance, but you can't assume that a certain method or property exists, just because it exists in the parent prototype / class. We want to find out, whether a given object fulfils the requirements to implement the "SantaClausable" interface. We need to implement a method which checks for this interface.
+
+# Rules
+
+# The SantaClausable interface is implemented, if all of the following methods are defined on an object:
+
+#     sayHoHoHo() / say_ho_ho_ho
+#     distributeGifts() / distribute_gifts
+#     goDownTheChimney() / go_down_the_chimney
+
+# Example
+
+# class SantaClaus
+#     def say_ho_ho_ho
+#         # Ho Ho Ho!
+#     end
+    
+#     def distribute_gifts
+#         # Gifts for all!
+#     end
+    
+#     def go_down_the_chimney
+#         # Whoosh!
+#     end
+# end
+  
+# class NotSantaClaus
+#     def say_ho_ho_ho
+#     end
+# end
+
+# is_santa_clausable(SantaClaus.new) # must return TRUE
+# is_santa_clausable(NotSantaClaus.new) # must return FALSE
+
+
+def is_santa_clausable(obj)
+  obj.class.method_defined?(:say_ho_ho_ho) && obj.class.method_defined?(:distribute_gifts) && obj.class.method_defined?(:go_down_the_chimney)
+end
+
+class SantaClaus
+  def say_ho_ho_ho
+    # Ho Ho Ho!
+  end
+  
+  def distribute_gifts
+    # Gifts for all!
+  end
+  
+  def go_down_the_chimney
+    # Whoosh!
+  end
+end
+
+class NotSantaClaus
+  def say_ho_ho_ho
+  end
+end
+
+p is_santa_clausable(SantaClaus.new) # => true
+p is_santa_clausable(NotSantaClaus.new) # => false
+
