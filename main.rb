fb636b161cac8bcc9ca356f59dcc4304abc7bb6f
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-06-20 01:50:37 +0000

    Complete codewars challenge 'Get the mean of an array'

--- main.rb
+++ main.rb
@@ -1616,3 +1616,24 @@ p count_bits(4) # => 1
 p count_bits(7) # => 3
 p count_bits(9) # => 2
 p count_bits(10) # => 2
+
+
+# Get the mean of an array
+
+# It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+# Return the average of the given array rounded down to its nearest integer.
+
+# The array will never be empty.
+
+
+def get_average(marks)
+  marks.sum/marks.size
+end
+
+
+
+p get_average([2, 2, 2, 2]) # => 2
+p get_average([1, 5, 87, 45, 8, 8]) # => 25
+p get_average([2,5,13,20,16,16,10]) # => 11
+p get_average([1,2,15,15,17,11,12,17,17,14,13,15,6,11,8,7]) # => 11
