995117f873ec153ff1c850c393b097cbb79653d7
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-06-23 02:30:54 +0000

    Complete codewars challenge 'SevenAte9'

--- main.rb
+++ main.rb
@@ -1723,3 +1723,24 @@ p solve(6) # => 132
 p solve(7) # => 429
 p solve(8) # => 1430
 p solve(20) # => 6564120420
+
+
+# SevenAte9
+
+# Write a function that removes every lone 9 that is inbetween 7s.
+
+# "79712312" --> "7712312"
+# "79797"    --> "777"
+
+
+def seven_ate9(str)
+  while str.include? "797"
+    str.sub!("797", "77")
+  end
+  return str
+end
+
+
+p seven_ate9('165561786121789797') # => '16556178612178977'
+p seven_ate9('797') # => '77'
+p seven_ate9('7979797') # => '7777'
