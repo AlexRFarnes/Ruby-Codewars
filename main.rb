f63a2b1561820644e0d2e6d6f814c761def01ad5
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-06-26 05:01:43 +0000

    Complete codewars challenge 'Difference of Volumes of Cuboids'

--- main.rb
+++ main.rb
@@ -1813,3 +1813,22 @@ p is_palindrome("Bob") # => true
 p is_palindrome("Madam") # => true
 p is_palindrome("AbBa") # => true
 p is_palindrome("") # => true
+
+
+# Difference of Volumes of Cuboids
+
+# In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+# For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+# Your function will be tested with pre-made examples as well as random ones.
+
+# If you can, try writing it in one line of code.
+
+
+def find_difference(a, b)
+  (a.reduce(:*) - b.reduce(:*)).abs
+end
+
+p find_difference([3, 2, 5], [1, 4, 4]) # => 14
+p find_difference([9, 7, 2], [5, 2, 2]) # => 106
