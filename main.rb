c1294df9ae7d64f4fda8dbced1cbe3b4872e4e36
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-06-27 00:47:44 +0000

    Complete codewara challenge 'Odd or Even?'

--- main.rb
+++ main.rb
@@ -1832,3 +1832,37 @@ end
 
 p find_difference([3, 2, 5], [1, 4, 4]) # => 14
 p find_difference([9, 7, 2], [5, 2, 2]) # => 106
+
+
+# Odd or Even?
+
+# Task:
+
+# Given a list of integers, determine whether the sum of its elements is odd or even.
+
+# Give your answer as a string matching "odd" or "even".
+
+# If the input array is empty consider it as: [0] (array with a zero).
+# Examples:
+
+# Input: [0]
+# Output: "even"
+
+# Input: [0, 1, 4]
+# Output: "odd"
+
+# Input: [0, -1, -5]
+# Output: "even"
+
+# Have fun!
+
+
+def odd_or_even(array)
+  array.sum.even? ? "even" : "odd"
+end
+
+p odd_or_even([0]) # => "even"
+p odd_or_even([1]) # => "odd"
+p odd_or_even([]) # => "even"
+p odd_or_even([-1023, 1, -2]) # => "even"
+p odd_or_even([-1023, -1, 3]) # => "odd"
