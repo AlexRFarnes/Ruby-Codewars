0fcc67e9309b16a75acf0b053934eb7ca7268ec1
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-07-06 07:27:43 +0000

    Complete codewars challenge 'Find the odd int'

--- main.rb
+++ main.rb
@@ -2191,3 +2191,28 @@ end
 p remove_url_anchor("www.codewars.com#about") # => "www.codewars.com"
 p remove_url_anchor("www.codewars.com/katas/?page=1#about") # => "www.codewars.com/katas/?page=1"
 p remove_url_anchor("www.codewars.com/katas/") # => "www.codewars.com/katas/"
+
+
+# Find the odd int
+
+# Given an array of integers, find the one that appears an odd number of times.
+
+# There will always be only one integer that appears an odd number of times.
+# Examples
+
+# [7] should return 7, because it occurs 1 time (which is odd).
+# [0] should return 0, because it occurs 1 time (which is odd).
+# [1,1,2] should return 2, because it occurs 1 time (which is odd).
+# [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+# [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+def find_it(seq)
+  seq.uniq.find { |num| seq.count(num).odd? }
+end
+
+
+p find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]) # => 5
+p find_it([1,1,2,-2,5,2,4,4,-1,-2,5]) # => -1
+p find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]) # => 5
+p find_it([10]) # => 10
+p find_it([1,1,1,1,1,1,10,1,1,1,1]) # => 10
