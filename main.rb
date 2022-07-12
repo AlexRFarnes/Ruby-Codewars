0333ea62193f0b9e4c3a15f075835c7b80b638ce
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-07-12 04:21:45 +0000

    Complete codewars challenge 'Who likes it?'

--- main.rb
+++ main.rb
@@ -2347,3 +2347,41 @@ p calc('aaaaaddddr') # => 30
 p calc('jfmgklf8hglbe') # => 6
 p calc('jaam') # => 12
 p calc('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') # => 96
+
+
+#  Who likes it?
+
+# You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+# Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+# []                                -->  "no one likes this"
+# ["Peter"]                         -->  "Peter likes this"
+# ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+# ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+# ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+# Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+def likes(names)
+  text = ""
+  if names.size == 0 
+    text = "no one likes this"
+  elsif names.size == 1
+    text = "#{names[0]} likes this"
+  elsif names.size == 2
+    text = "#{names[0]} and #{names[1]} like this"
+  elsif names.size == 3
+    text = "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  else
+    text = "#{names[0]}, #{names[1]} and #{names.size-2} others like this"
+  end
+  return text
+end
+
+
+p likes([]) # => 'no one likes this'
+p likes(['Peter']) # => 'Peter likes this'
+p likes(['Jacob', 'Alex']) # => 'Jacob and Alex like this'
+p likes(['Max', 'John', 'Mark']) # => 'Max, John and Mark like this'
+p likes(['Alex', 'Jacob', 'Mark', 'Max']) # => 'Alex, Jacob and 2 others like this'
