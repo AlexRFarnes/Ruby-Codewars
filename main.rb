71c7a5a020d7d732b850120073ccd8c20ad7ec9d
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-07-15 09:26:25 +0000

    Complete codewars challenge 'Friend or Foe?'

--- main.rb
+++ main.rb
@@ -2474,3 +2474,30 @@ p pyramid_height(5525) # =>  25
 p pyramid_height(6200) # =>  25
 p pyramid_height(6201) # =>  26
 p pyramid_height(6254) # =>  26
+
+
+
+# Friend or Foe?
+
+#   Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+# If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+# Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+# i.e.
+
+# friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+
+# Note: keep the original order of the names in the output.
+
+  
+def friend(friends)
+  friends.reject { |name| name.size != 4 }
+end
+
+
+p friend(["Ryan", "Kieran", "Mark"]) # => ["Ryan", "Mark"]
+p friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]) # => ["Ryan"]
+p friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]) # => ["Jimm", "Cari", "aret"]
+p friend(["Love", "Your", "Face", "1"]) # => ["Love", "Your", "Face"]
