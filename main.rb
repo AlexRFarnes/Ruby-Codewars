59e3d0c1312431eb75c6a0222af02aaffa649512
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-07-18 01:29:42 +0000

    Complete challenge 'Terminal game move function'

--- main.rb
+++ main.rb
@@ -2521,3 +2521,24 @@ p find_function([9,3,lambda{|a| a%2!=0},1,0],[1,2,3,4]) # => [1,3])
 p find_function([9,3,lambda{|a| a%13==0},1,0],[1,2,3,4]) # => [])
 p find_function([9,3,lambda{|a| a%13!=0},1,0],[1,2,3,4]) # => [1,2,3,4])
 p find_function([5,'a',lambda{|a| a*4!=0},1,0],[0,1,2,3,4]) # => [1,2,3,4])
+
+
+# Grasshopper - Terminal game move function
+
+# Terminal game move function
+
+# In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+# Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+# Example:
+
+# move(3, 6) should equal 15
+
+def move (position, roll)
+  position += roll*2
+end
+
+
+p move(0,4) # =>  8
+p move(3, 6) # =>  15
+p move(2, 5) # =>  12
