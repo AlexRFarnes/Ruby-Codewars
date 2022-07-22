399c46e65c33ec4928c432166a6041f053da072d
Alexander R. Farnes <57517804+AlexRFarnes@users.noreply.github.com>
2022-07-22 03:05:34 +0000

    Complete challenge 'Predict your age!'

--- main.rb
+++ main.rb
@@ -2634,3 +2634,32 @@ s = 19
 u = 0
 p gps(s, x) 
 p u
+
+
+# Predict your age!
+
+# My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+# In honor of my grandfather's memory we will write a function using his formula!
+
+#   Take a list of ages when each of your great-grandparent died.
+#   Multiply each number by itself.
+#   Add them all together.
+#   Take the square root of the result.
+#   Divide by two.
+
+# Example
+
+# predict_age(65, 60, 75, 55, 60, 63, 64, 45) == 86
+
+# Note: the result should be rounded down to the nearest integer.
+
+# Some random tests might fail due to a bug in the JavaScript implementation. Simply resubmit if that happens to you.
+
+def predict_age(*args)
+  (Math.sqrt(args.map { |age| age * age }.sum)/2).to_i
+end
+
+
+p predict_age(65,60,75,55,60,63,64,45) # => 86
+p predict_age(32,54,76,65,34,63,64,45) # => 79
